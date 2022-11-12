7a350cb2cb8655aa2fd0fba4bc2c96d228d2dc69
romildoalimajr <odlimor.sevla@gmail.com>
2022-11-11 23:20:24 -0300

    new file:   lib/components/meal_item.dart 	modified:   lib/data/dummy_data.dart 	modified:   lib/main.dart 	modified:   lib/models/meal.dart 	modified:   lib/screens/categories_meals_screen.dart 	new file:   lib/screens/meal_detail_screen.dart 	modified:   lib/utils/app_routes.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'screens/categories_screen.dart';
 import 'screens/categories_meals_screen.dart';
 import 'utils/app_routes.dart';
+import 'screens/meal_detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,8 +31,9 @@ class MyApp extends StatelessWidget {
             ),
       ),
       routes: {
-        AppRoutes.HOME: (ctx) => CategoriesScreen(),
+        AppRoutes.HOME: (ctx) => const CategoriesScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
       },
     );
   }
